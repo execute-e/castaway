b5b515145e141e0027c3876bb9f04bb73a4ce366
executed <execute.eee@yandex.ru>
2025-05-06 22:54:31 +0300

    fix

--- src/scripts/Header.js
+++ src/scripts/Header.js
@@ -1,29 +1,28 @@
-export default class Header {
+class Header {
     selectors = {
         root: '[data-js-header]',
         overlay: '[data-js-header-overlay]',
-        button: '[data-js-header-burger-button]'
+        burgerButton: '[data-js-header-burger-button]',
     }
 
     stateClasses = {
-        isActive: 'is-active'
+        isActive: 'is-active',
     }
 
     constructor() {
         this.rootElement = document.querySelector(this.selectors.root);
-        this.overlay = this.rootElement.querySelector(this.selectors.overlay);
-        this.button = this.rootElement.querySelector(this.selectors.button);
-        this.bindEvents();
-        console.log(this.rootElement, this.overlay, this.button);
+        this.overlayElement = this.rootElement.querySelector(this.selectors.overlay);
+        this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+        this.bindEvents()
     }
 
     onBurgerButtonClick = () => {
-        //this.button.classList.toggle(this.stateClasses.isActive);
-        this.overlay.classList.toggle(this.stateClasses.isActive);
+        this.overlayElement.classList.toggle(this.stateClasses.isActive);
     }
 
     bindEvents() {
-        this.button.addEventListener('click', this.onBurgerButtonClick);
+        this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
     }
+}
 
-};
+export default Header;
